98dd1b60cf72571b273e5c8efcd60f7d13ab3bd9
Helga Rais <cdg013@gmail.com>
2018-09-13 02:30:26 +0300

    Задание из недели 2 - модуль ephem

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 from settings import PROXY, API_KEY
+import ephem, datetime
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -12,6 +13,20 @@ def greet_user(bot, update):
     text = 'Вызван /start'
     logging.info(text)
     update.message.reply_text(text)
+
+
+def about_planet(bot, update):
+    message = update.message.text
+    logging.info(message)
+    planet_name = message.split(' ')[1]
+    try:
+        planet_info = getattr(ephem, planet_name)(datetime.datetime.today())
+        constellation = ephem.constellation(planet_info)
+        update.message.reply_text(constellation[1])
+    except AttributeError:
+        update.message.reply_text('Planet "{}" doesn\'t exist! Try any of those: Mercury, '
+                                  'Venus, Mars, Jupiter, Saturn, Uranus, '
+                                  'Neptune'.format(planet_name))
 
 
 def talk_to_me(bot, update):
@@ -27,9 +42,11 @@ def main():
     logging.info('Бот запускается')
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", about_planet))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     mybot.start_polling()
     mybot.idle()
 
 
-main()
+if __name__ == "__main__":
+    main()
